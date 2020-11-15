741b7432fc34f97d6433d7a51993fa260059d051
linjj <linjj@amtxts.com>
2020-11-15 16:24:10 +0800

    InsertionSort(插入排序)的降序排序，从后往前排

--- src/Sort/InsertionSort.java
+++ src/Sort/InsertionSort.java
@@ -35,8 +35,32 @@ public class InsertionSort {
         }
     }
 
+    /**
+     * 降序
+     * [0,i)未排序，[i,n]已排序，从后往前遍历
+     *
+     * @param arr 数组
+     * @author ljj
+     * @date 2020/11/15
+     */
+    public static <E extends Comparable<E>> void descendingSort(E[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            E t = arr[i];
+            int j;
+            for (j = i; j < arr.length - 1; j++) {
+                if (t.compareTo(arr[j + 1]) < 0) {
+                    arr[j] = arr[j + 1];
+                } else {
+                    break;
+                }
+            }
+            arr[j] = t;
+        }
+    }
+
     public static void main(String[] args) {
         Integer[] arr = ArrayGenerator.generateRandomArray(5, 5);
         SortingHelper.sortTest(InsertionSort.class.getName(), arr);
+        InsertionSort.descendingSort(arr);
     }
 }
